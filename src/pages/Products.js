9c99d37622924370f2722b543b379ec1298f2538
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-22 04:05:12 +0200

    Adding Dynamic Routes with Params

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Products() {
-  return <div>Products</div>;
+  return (
+    <section>
+      <h1>The Product Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An Online Course</li>
+      </ul>
+    </section>
+  );
 }
 
 export default Products;
